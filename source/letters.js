324fd6783137862970e8d6b697734bb444fc6181
yuliaplaksina <plaksina-yulia@yandex.ru>
2019-09-02 16:21:26 +0300

    Внесены изменения в соответствии с правками

--- source/letters.js
+++ source/letters.js
@@ -1,29 +1,35 @@
 'use strict';
 
-function letters(string, mode = null) {
-    if (mode) {
-        return Array.from(new Set(Array.from(string))).join("");
+function letters(string, mode) {
+    let newString;
+
+    switch (mode) {
+        case true:
+            const uniqueMode1 = new Set(Array.from(string));
+            newString = Array.from(uniqueMode1).join('');
+            break;
+
+        case false:
+            const uniqueMode2 = new Set(Array.from(string).reverse());
+            newString = Array.from(uniqueMode2).reverse().join('');
+            break;
+
+        default:
+            const letters = Array.from(string);
+            const map = new Map();
+
+            for (let letter of letters) {
+                if (map.has(letter)) {
+                    const letterCount = map.get(letter);
+                    map.set(letter, letterCount + 1);
+                } else {
+                    map.set(letter, 1);
+                }
+            }
+
+            newString = letters.filter(letter => map.get(letter) < 2).join('');
+            break;
     }
-    if (mode === false) {
-        return Array.from(new Set(Array.from(string).reverse())).reverse().join("");
-    }
 
-    let letters = Array.from(string);
-
-    let map = new Map();
-
-    for (let letter of letters) {
-        if (!map.has(letter)) {
-            map.set(letter, +1);
-        } else {
-            map.set(letter, map.get(letter) + 1);
-        }
-    }
-
-    return letters.filter( letter => {
-        return map.get(letter) < 2;
-    }).join("");
-
-
-
+    return newString;
 }
